1d705da6052c7989679a1288280336101f194828
khaleelsyed <khaleelsyed@users.noreply.github.com>
2024-08-09 23:15:09 +0100

    update layout

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -7,37 +7,37 @@ import { Theme } from "./util/theme"
 export type Analytics =
   | null
   | {
-      provider: "plausible"
-      host?: string
-    }
+    provider: "plausible"
+    host?: string
+  }
   | {
-      provider: "google"
-      tagId: string
-    }
+    provider: "google"
+    tagId: string
+  }
   | {
-      provider: "umami"
-      websiteId: string
-      host?: string
-    }
+    provider: "umami"
+    websiteId: string
+    host?: string
+  }
   | {
-      provider: "goatcounter"
-      websiteId: string
-      host?: string
-      scriptSrc?: string
-    }
+    provider: "goatcounter"
+    websiteId: string
+    host?: string
+    scriptSrc?: string
+  }
   | {
-      provider: "posthog"
-      apiKey: string
-      host?: string
-    }
+    provider: "posthog"
+    apiKey: string
+    host?: string
+  }
   | {
-      provider: "tinylytics"
-      siteId: string
-    }
+    provider: "tinylytics"
+    siteId: string
+  }
   | {
-      provider: "cabin"
-      host?: string
-    }
+    provider: "cabin"
+    host?: string
+  }
 
 export interface GlobalConfiguration {
   pageTitle: string
@@ -83,5 +83,5 @@ export interface FullPageLayout {
   footer: QuartzComponent
 }
 
-export type PageLayout = Pick<FullPageLayout, "beforeBody" | "left" | "right">
+export type PageLayout = Pick<FullPageLayout, "beforeBody" | "left" | "right" | "afterBody">
 export type SharedLayout = Pick<FullPageLayout, "head" | "header" | "footer" | "afterBody">
